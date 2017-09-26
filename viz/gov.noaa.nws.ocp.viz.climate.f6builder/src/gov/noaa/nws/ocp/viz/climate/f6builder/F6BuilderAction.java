c3082b87a10e1235a0ea016d06e90c32cbbe1c91
Andrew Moore <Andrew.S.Moore@noaa.gov>
2017-09-26 14:33:32 +0000

    VLab Issue #30123 - Climate F6 product builder; fixes #30123
    Change-Id: I491afc3113254cc617c85123fc449c2ff74f8183
    (cherry picked from commit cb2f36a814d0382967bd1cd848a0e34795b5f03a)

---
+++ viz/gov.noaa.nws.ocp.viz.climate.f6builder/src/gov/noaa/nws/ocp/viz/climate/f6builder/F6BuilderAction.java
@@ -0,0 +1,59 @@
+/**
+ * This software was developed and / or modified by NOAA/NWS/OCP/ASDT
+ **/
+package gov.noaa.nws.ocp.viz.climate.f6builder;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+
+import com.raytheon.viz.ui.dialogs.ICloseCallback;
+
+/**
+ * F6 builder action, to bring up F6 builder dialog.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Dec 7, 2015             xzhang      Initial creation
+ * 18 AUG 2016  20996      amoore      Make singleton.
+ * 
+ * </pre>
+ * 
+ * @author xzhang
+ * @version 1.0
+ */
+
+public class F6BuilderAction extends AbstractHandler {
+
+    private F6BuilderDialog f6BuilderDialog;
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+
+        Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+                .getShell();
+        if (f6BuilderDialog == null
+                || f6BuilderDialog.getShell().isDisposed()) {
+            f6BuilderDialog = new F6BuilderDialog(shell);
+            f6BuilderDialog.setCloseCallback(new ICloseCallback() {
+
+                @Override
+                public void dialogClosed(Object returnValue) {
+                    f6BuilderDialog = null;
+                }
+            });
+            f6BuilderDialog.open();
+        } else {
+            f6BuilderDialog.bringToTop();
+        }
+
+        return null;
+    }
+
+}
