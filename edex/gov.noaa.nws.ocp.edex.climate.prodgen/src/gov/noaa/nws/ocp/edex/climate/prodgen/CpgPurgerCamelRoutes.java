fae0fed62fd39e7724fe48f2cf317172fa80421a
Lisa Singh <lisa.singh@noaa.gov>
2024-08-08 13:25:08 -0400

    Issue #2037701 - Converted geodata, progen, metaroclimate, atcf, asos, Edex ingest routes for Camel 4
    Routes converted:
    
    - geodata
    - ocp climate progen
    - metaroclimate
    - atcf
    - climate asos
    
    Change-Id: Ifed9fce281cb4ec4f61fbb03168a861856ba5aa7

---
+++ edex/gov.noaa.nws.ocp.edex.climate.prodgen/src/gov/noaa/nws/ocp/edex/climate/prodgen/CpgPurgerCamelRoutes.java
@@ -0,0 +1,75 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ *
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ *
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ *
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+
+package gov.noaa.nws.ocp.edex.climate.prodgen;
+
+import com.raytheon.uf.edex.esb.camel.EDEXRouteBuilder;
+
+/**
+ * Camel routes converted from file "cpg-spring.xml", context "cpg_purger-camel"
+ *
+ * <pre>
+ *
+ * SOFTWARE HISTORY
+ *
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * 2024-07-11   2037701    aford       Initial creation (from auto-generated)
+ *
+ * </pre>
+ */
+
+public class CpgPurgerCamelRoutes extends EDEXRouteBuilder {
+
+    private final String cpgPurgerCron;
+
+    private final String recordPurgerCron;
+
+    public CpgPurgerCamelRoutes(String cpgPurgerCron, String recordPurgerCron) {
+        this.cpgPurgerCron = cpgPurgerCron;
+        this.recordPurgerCron = recordPurgerCron;
+    }
+
+    @Override
+    public void configure() throws Exception {
+        // @formatter:off
+
+        // purge CPG sessions table
+        from("quartz://cpg/cpgSessionPurger/?cron=" + this.cpgPurgerCron)
+                .doTry()
+                        .bean("climateCPGSessionPurger", "purgeTerminatedCPGSession")
+                .doCatch(Throwable.class)
+                        .to("log:cpgSessionPurger?level=ERROR")
+                .endDoTry()
+                .end()
+                .setId("climateCPGPurgeWork");
+
+        // purge sent_prod_record table
+        from("quartz://cpg/sentRecordPurger/?cron=" + this.recordPurgerCron)
+                .doTry()
+                        .bean("climateSentRecordPurger", "purgeSentProductRecords")
+                .doCatch(Throwable.class)
+                        .to("log:cpgSessionPurger?level=ERROR")
+                .endDoTry()
+                .end()
+                .setId("climateSentRecordPurgeWork");
+        // @formatter:on
+    }
+}
---
+++ edex/gov.noaa.nws.ocp.edex.metartoclimate/src/gov/noaa/nws/ocp/edex/metartoclimate/MetarToClimateSvrCamelRoutes.java
@@ -0,0 +1,63 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ *
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ *
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ *
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+
+package gov.noaa.nws.ocp.edex.metartoclimate;
+
+import com.raytheon.uf.edex.esb.camel.EDEXRouteBuilder;
+
+/**
+ * Camel routes converted from file "metartoclimate-spring.xml", context
+ * "metarToClimateSvr-camel"
+ *
+ * <pre>
+ *
+ * SOFTWARE HISTORY
+ *
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * 2024-07-11   2037701    aford       Initial creation (from auto-generated)
+ *
+ * </pre>
+ */
+
+public class MetarToClimateSvrCamelRoutes extends EDEXRouteBuilder {
+
+    private final String metartoclimatepurgeCron;
+
+    public MetarToClimateSvrCamelRoutes(String metartoclimatepurgeCron) {
+        this.metartoclimatepurgeCron = metartoclimatepurgeCron;
+    }
+
+    @Override
+    public void configure() throws Exception {
+        // @formatter:off
+
+        // purge climate DB rpt table
+        from("quartz://metartoclimate/climatepurge/?cron=" + this.metartoclimatepurgeCron)
+                .doTry()
+                        .bean("metarToClimatePurger", "purgeClimateReport")
+                .doCatch(Throwable.class)
+                        .to("log:metartoclimate?level=ERROR")
+                .endDoTry()
+                .end()
+                .setId("metarClimatePurgeWork");
+        // @formatter:on
+    }
+}
