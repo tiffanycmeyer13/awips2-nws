343c07a9f3c5f896ff7534c0d3ab0245e3405256
derek.haines <derek.haines@noaa.gov>
2025-12-03 14:09:45 -0500

    Issue #2030113 Replace org.apache.commons.lang with lang3
    commons lang 2.x is obsolete.
    
    This commit contains only changes to import statements and MANIFEST.MF.
    
    Change-Id: Ia1d93df87eaf81b6eb7c76f8c5d7081e5e4c2564
    (cherry picked from commit e1cae6773dac42f9637a3054896e0180dceff6ca)

--- viz/gov.noaa.nws.ocp.viz.psh/src/gov/noaa/nws/ocp/viz/psh/ui/generator/tab/PshRainfallTabComp.java
+++ viz/gov.noaa.nws.ocp.viz.psh/src/gov/noaa/nws/ocp/viz/psh/ui/generator/tab/PshRainfallTabComp.java
@@ -16,7 +16,7 @@ import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Locale;
 
-import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
 import org.eclipse.swt.custom.SashForm;
